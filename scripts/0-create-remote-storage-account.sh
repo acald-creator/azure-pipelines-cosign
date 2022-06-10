12bd151c3c9daf6e31f415499614086315702af5
Antonette Caldwell <pullmana8@gmail.com>
2022-06-09 21:17:46 -0500

    Renamed variables for readability again.

--- scripts/0-create-remote-storage-account.sh
+++ scripts/0-create-remote-storage-account.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-RESOURCE_GROUP_NAME=tfstate
-AZURE_STORAGE_ACCOUNT_NAME=tfstate$RANDOM
-CONTAINER_NAME=tfstate
+export AZURE_RESOURCE_GROUP_NAME=tfstate
+export AZURE_STORAGE_ACCOUNT_NAME=tfstate$RANDOM
+export AZURE_STORAGE_CONTAINER_NAME=tfstate
 
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location eastus
+az group create --name $AZURE_RESOURCE_GROUP_NAME --location eastus
 
 # Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $AZURE_STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob --access-tier Hot
+az storage account create --resource-group $AZURE_RESOURCE_GROUP_NAME --name $AZURE_STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob --access-tier Hot
 
 # Query blob endpoint
-az storage account show -n $AZURE_STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME --query "primaryEndpoints.blob"
+az storage account show -n $AZURE_STORAGE_ACCOUNT_NAME -g $AZURE_RESOURCE_GROUP_NAME --query "primaryEndpoints.blob"
 
 # Query the first storage key
-az storage account keys list --resource-group $RESOURCE_GROUP_NAME -n $AZURE_STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv
+az storage account keys list --resource-group $AZURE_RESOURCE_GROUP_NAME -n $AZURE_STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv
 
 STORAGE_ACCOUNT_ID=az storage account list --query "[].id" -o tsv
 SIGNED_IN_USER_OBJECT_ID=az ad signed-in-user show --query objectId
@@ -22,4 +22,4 @@ SIGNED_IN_USER_OBJECT_ID=az ad signed-in-user show --query objectId
 az role assignment create --assignee $(az ad signed-in-user show --query objectId) --role "Storage Blob Data Contributor" --scope $(az storage account list --query "[].id" -o tsv)
 
 # Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $AZURE_STORAGE_ACCOUNT_NAME
+az storage container create --name $AZURE_STORAGE_CONTAINER_NAME --account-name $AZURE_STORAGE_ACCOUNT_NAME
